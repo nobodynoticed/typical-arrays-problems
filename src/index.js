258332ab9bef9997c47ceb9a466c8dc6f840364e
nobodynoticed <eugenejukov23@gmail.com>
2021-03-10 15:17:38 +0300

    implement typical-array-problems func

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
+exports.min = function min(array) {
+    if (!Array.isArray(array) || !array.length) {
+        return 0;
+    }
 
-exports.min = function min (array) {
-  return 0;
-}
+    return Math.min(...array);
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!Array.isArray(array) || !array.length) {
+        return 0;
+    }
 
-exports.avg = function avg (array) {
-  return 0;
-}
+    return Math.max(...array);
+};
+
+exports.avg = function avg(array) {
+    if (!Array.isArray(array) || !array.length) {
+        return 0;
+    }
+
+    const result = array.reduce((acc, num) => {
+        const res = acc + num;
+        return res;
+    }, 0);
+    return result / array.length;
+};
